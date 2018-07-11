2ae899872b985b364c833c91e8f45e29cb9f2efa
Den Chou <denchou@Dens-MBP.fios-router.home>
2018-07-11 14:10:44 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,24 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+var flipCard = function(cardId){
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
 
 if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You've found a match!");
-	}
-	else{
-		alert("Sorry, try again!");
-	}
+checkForMatch();
 }
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
+};
+
+flipCard(0);
+flipCard(2);
+
